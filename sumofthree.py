af9bd0ea216b67c3b4190884387ca585a43b7fba
achui1014 <ashleyytchui@gmail.com>
2026-01-20 20:34:22 -0500

    a0

---
+++ sumofthree.py
@@ -0,0 +1,21 @@
+##
+## *******************************************************************
+## Ashley Chui (21003047)
+## CS 231 Spring 2024
+## Assignment 0, P1
+## *******************************************************************
+##
+
+
+def sum_of_three(num):
+    '''
+    sum_of_three(num) produces the sum of three integers, num, 27, and
+      42.
+    sum_of_three: Int -> Int
+
+    Examples:
+      sum_of_three(-2) => 67
+      sum_of_three(0) => 69
+      sum_of_three(1) => 70
+    '''
+    return num + 27 + 42
