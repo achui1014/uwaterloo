927d8f6e7fa0df6bd0bf18454038cbfe03aa3383
Ashley Chui <ashleyytchui@gmail.com>
2026-03-24 00:19:25 -0400

    STAT332 paper frogs data

---
+++ STAT332/a3/paper_frogs.r
@@ -0,0 +1,14 @@
+frog_jumps <- data.frame(
+    dimension = c(
+        rep(10, 10),
+        rep(15, 10),
+        rep(20, 10)
+    ),
+    distance  = c(
+        24, 20, 30, 36, 21, 23, 42, 19, 27, 26,
+        26, 21, 27, 17, 22, 21, 18, 43, 30, 24,
+        35, 22, 36, 44, 29, 36, 35, 30, 32, 27
+    )
+)
+frog_jumps$dimension <- as.factor(frog_jumps$dimension)
+
